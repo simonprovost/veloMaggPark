c5da05b81641febef56e0c055b8bcc37dc85bc7f
Simon <simon1.provost@epitech.eu>
2020-03-22 18:38:31 +0100

    feat/clean(master): refactoring callout + icon content + markers, optimization and eject from expo to native app.

--- App.js
+++ App.js
@@ -1,135 +1,143 @@
 import React, {Component} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
-import Map from './app/components/Map'
-import {Header, Text} from "react-native-elements";
-import {AppLoading,} from "expo";
-import {default as XMLParser_} from "react-xml-parser"
-
+import Map from './app/components/Map';
+import {Header, Text} from 'react-native-elements';
 
 class App extends Component {
-	state = {
-		parkMarkers:  [],
-		loading: true,
-	};
+  state = {
+    parkMarkers: [],
+    loading: true,
+  };
 
-	componentWillMount() {
-	}
+  static getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
-	static getRandomColor() {
-		let letters = '0123456789ABCDEF';
-		let color = '#';
-		for (let i = 0; i < 6; i++)
-			color += letters[Math.floor(Math.random() * 16)];
-		return color;
-	}
+  componentDidMount() {
+    this._allParkPos();
+  }
 
-	componentDidMount() {
-		this._allParkPos();
-	};
+  _allParkPos = () => {
+    let array = [];
+    let XMLParser = require('react-xml-parser');
 
-	_allParkPos =  () => {
-		let array = [];
-		let XMLParser = require('react-xml-parser');
+    fetch(
+      'https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml',
+    )
+      .then(response => response.text())
+      .then(response => {
+        const xml = new XMLParser().parseFromString(response); // Assume xmlText contains the example XML
+        for (let i = 0; i < xml.getElementsByTagName('si').length; i++) {
+          let occupiedPlaces = xml.getElementsByTagName('si')[i].attributes.av;
+          let creditCardReader = xml.getElementsByTagName('si')[i].attributes
+            .cb;
+          let availablePlaces = xml.getElementsByTagName('si')[i].attributes.fr;
+          let latitude = xml.getElementsByTagName('si')[i].attributes.la;
+          let longitude = xml.getElementsByTagName('si')[i].attributes.lg;
+          let stationName = xml.getElementsByTagName('si')[i].attributes.na;
+          let totalPlaces = xml.getElementsByTagName('si')[i].attributes.to;
 
-		fetch('https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml')
-			.then(response => response.text())
-			.then((response) => {
-				const xml = new XMLParser().parseFromString(response);    // Assume xmlText contains the example XML
-				for (let i = 0 ; i < xml.getElementsByTagName('si').length ; i++) {
+          let output = '';
+          output += 'Nombre total de places:' + totalPlaces + '\n';
+          output += 'Nombre de places occupées:' + occupiedPlaces + '\n';
+          output += 'Nombre de place libres:' + availablePlaces + '\n';
 
-					let occupiedPlaces = xml.getElementsByTagName('si')[i]['attributes']['av'];
-					let creditCardReader = xml.getElementsByTagName('si')[i]['attributes']['cb'];
-					let availablePlaces = xml.getElementsByTagName('si')[i]['attributes']['fr'];
-					let latitude = xml.getElementsByTagName('si')[i]['attributes']['la'];
-					let longitude = xml.getElementsByTagName('si')[i]['attributes']['lg'];
-					let stationName = xml.getElementsByTagName('si')[i]['attributes']['na'];
-					let totalPlaces = xml.getElementsByTagName('si')[i]['attributes']['to'];
+          if (creditCardReader === '1') {
+            output += 'Lecteur de CB disponible' + '\n';
+          } else {
+            output += 'Lecteur de CB non disponible' + '\n';
+          }
+          array.push({
+            title: stationName.substr(stationName.indexOf(' ') + 1),
+            occupiedPlaces: occupiedPlaces,
+            availablePlaces: availablePlaces,
+            creditCardReader: creditCardReader,
+            color: App.getRandomColor(),
+            coordinates: {
+              latitude: parseFloat(latitude),
+              longitude: parseFloat(longitude),
+            },
+          });
+        }
+        this.setState({loading: false});
+      })
+      .catch(err => {
+        alert('an error was occured. Try to restart the app.');
+      });
+    this.setState({parkMarkers: array});
+  };
 
-					let output = "";
-					output += "Nombre total de places:" + totalPlaces + "\n";
-					output += "Nombre de places occupées:" + occupiedPlaces + "\n";
-					output += "Nombre de place libres:" + availablePlaces + "\n";
+  render() {
+    const {parkMarkers, loading} = this.state;
 
-					if (creditCardReader === "1")
-						output += "Lecteur de CB disponible" + "\n";
-					else
-						output += "Lecteur de CB non disponible" + "\n";
+    if (loading) {
+      return (
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
 
-					array.push({
-						title: stationName.substr(stationName.indexOf(' ') + 1),
-						occupiedPlaces: occupiedPlaces,
-						availablePlaces: availablePlaces,
-						creditCardReader: creditCardReader,
-						color: App.getRandomColor(),
-						coordinates: {
-							latitude: parseFloat(latitude),
-							longitude: parseFloat(longitude),
-						},
-					});
-				}
-				this.setState({loading: false})
-			}).catch((err) => {
-				alert("an error was occured. Try to restart the app.")
-		});
-		this.setState({parkMarkers: array,});
-	};
-
-	render () {
-		const {parkMarkers, loading} = this.state;
-
-		if (loading) {
-			return (
-				<View>
-					<AppLoading />
-				</View>
-			);
-		}
-
-		return (
-			<View style={styles.container}>
-				<Header
-					statusBarProps={{ barStyle: 'light-content' }}
-					barStyle="light-content"
-					centerComponent={<Text h4 style={styles.titleHeader}>{"Velo Magg Park"}</Text>}
-					containerStyle={styles.header}
-				/>
-				<SafeAreaView style={{	width: '100%',
-					height: '100%', marginTop: '20%'}}>
-					<Map region={region} places={parkMarkers} />
-				</SafeAreaView>
-			</View>
-		);
-	}
+    return (
+      <View style={styles.container}>
+        <Header
+          statusBarProps={{barStyle: 'light-content'}}
+          barStyle="light-content"
+          centerComponent={
+            <Text h4 style={styles.titleHeader}>
+              {'Velo Magg Park'}
+            </Text>
+          }
+          containerStyle={styles.header}
+        />
+        <SafeAreaView style={styles.map}>
+          <Map region={region} places={parkMarkers} />
+        </SafeAreaView>
+      </View>
+    );
+  }
 }
 
 // A placeholder until we get our own location
 const region = {
-	latitude: 43.610769,
-	longitude: 3.876716,
-	latitudeDelta: 0.0922,
-	longitudeDelta: 0.0421
+  latitude: 43.610769,
+  longitude: 3.876716,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
 };
 
-
 const styles = StyleSheet.create({
-	container: {
-		width: '100%',
-		height: '100%',
-	},
-	container2: {
-		flex: 1,
-	},
-	header: {
-		backgroundColor: '#6495ED',
-		justifyContent: 'space-around',
-		height: "10%",
-		position: 'absolute',
-		top: 0, left: 0, right: 0,
-		zIndex: 1,
-	},
-	titleHeader: {
-		color: 'white',
-	},
+  container: {
+    flex:1,
+    flexDirection: 'column',
+  },
+  container2: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#6495ED',
+    justifyContent: 'space-around',
+    height: '10%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1,
+  },
+  titleHeader: {
+    color: 'white',
+    fontFamily: 'Roboto',
+  },
+  map: {
+    flex: 1,
+    marginTop: '20%',
+    marginBottom: '-10%',
+  },
 });
 
 export default App;
--- app/components/Map.js
+++ app/components/Map.js
@@ -1,162 +1,153 @@
-import React, { Component } from 'react'
-import {View, Text, Button, TouchableOpacity, Image, SafeAreaView, TouchableHighlight} from 'react-native'
-import { Icon } from 'react-native-elements'
-import {Marker, AnimatedRegion, Callout} from 'react-native-maps';
-import MapView from "react-native-map-clustering";
+import React, {Component} from 'react';
+import {View, Text, Image} from 'react-native';
+import {Icon} from 'react-native-elements';
+import {Marker, Callout} from 'react-native-maps';
+import MapView from 'react-native-map-clustering';
 
 export const getCurrentLocation = () => {
-	return new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
-	});
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      position => resolve(position),
+      e => reject(e),
+    );
+  });
 };
 
 class CustomCalloutView extends React.Component {
+  render() {
+    let creditCard =
+      this.props.creditCardReader === '1' ? (
+        <View style={styles.bottomLeftIcon}>
+          <Icon
+            name="cc-visa"
+            type={'font-awesome'}
+            color="#6495ED"
+            size={28}
+          />
+        </View>
+      ) : (
+        <View />
+      );
 
-	render() {
-		let creditCard = this.props.creditCardReader === '1' ? <View style={styles.bottomLeftIcon}>
-			<Icon
-				name='cc-visa'
-				type={'font-awesome'}
-				color='#6495ED'
-				size={28}
-			/>
-		</View> : <View/>;
+    return (
+      <View style={styles.popupView}>
+        <Text style={styles.titlePopup}>{this.props.title}</Text>
 
-		return (
-			<View style={styles.popupView}>
-				<Text style={styles.titlePopup}>
-					{this.props.title}
-				</Text>
-
-				<View style={styles.bottomLeftSide}>
-					<Text style={styles.descrPopup}>
-						{this.props.occupiedPlaces}
-					</Text>
-					<View style={styles.bottomLeftIcon}>
-						<Icon
-							name='ios-bicycle'
-							type='ionicon'
-							color='#6495ED'
-						/>
-					</View>
-					<View style={styles.separator}>
-						<Icon
-							name='ellipsis-v'
-							type={'font-awesome'}
-							color='grey'
-						/>
-					</View>
-					<Text style={styles.descrPopup}>
-						{this.props.availablePlaces}
-					</Text>
-					<Icon
-						name='local-parking'
-						color='#6495ED'
-						size={28}
-					/>
-					{creditCard}
-				</View>
-			</View>
-		)
-	}
+        <View style={styles.bottomLeftSide}>
+          <Text style={styles.descrPopup}>{this.props.occupiedPlaces}</Text>
+          <View style={styles.bottomLeftIcon}>
+            <Icon name="ios-bicycle" type="ionicon" color="#6495ED" />
+          </View>
+          <View style={styles.separator}>
+            <Icon name="ellipsis-v" type={'font-awesome'} color="grey" />
+          </View>
+          <Text style={styles.descrPopup}>{this.props.availablePlaces}</Text>
+          <Icon name="local-parking" color="#6495ED" size={28} />
+          {creditCard}
+        </View>
+      </View>
+    );
+  }
 }
 
-export default class Map extends Component  {
-	state = {
-		bottomMargin: 1,
-		region:  {
-			latitude: 43.610769,
-			longitude: 3.876716,
-			latitudeDelta: 0.0922,
-			longitudeDelta: 0.0421
-		},
-	};
+export default class Map extends Component {
+  state = {
+    bottomMargin: 1,
+    region: {
+      latitude: 43.610769,
+      longitude: 3.876716,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  };
 
-	renderMarkers() {
-		return this.props.places.map((place, i) => (
-			<Marker
-				key={i} coordinate={place.coordinates}
-				pinColor={place.color} opacity={0.8}
-			>
-				<Icon
-					name='ios-bicycle'
-					type='ionicon'
-					color={place.color}
-					size={30}
-				/>
+  renderMarkers() {
+    return this.props.places.map((place, i) => (
+      <Marker
+        key={i}
+        coordinate={place.coordinates}
+        pinColor={place.color}
+        opacity={0.8}>
+        {/* <Image
+          source={require('./../../assets/bicycle_1.png')}
+          style={{
+            width: 28,
+            height: 28,
+            resizeMode: 'contain',
+            tintColor: place.color,
+          }}
+        />
+*/}
 
-				<Callout tooltip={true}>
-					<CustomCalloutView
-						title={place.title}
-						availablePlaces={place.availablePlaces}
-						occupiedPlaces={place.occupiedPlaces}
-						creditCardReader={place.creditCardReader}
-					/>
-				</Callout>
-			</Marker>
+        <Icon name="ios-bicycle" type="ionicon" color={place.color} size={30} />
 
-		))
-	}
+        <Callout tooltip={true}>
+          <CustomCalloutView
+            title={place.title}
+            availablePlaces={place.availablePlaces}
+            occupiedPlaces={place.occupiedPlaces}
+            creditCardReader={place.creditCardReader}
+          />
+        </Callout>
+      </Marker>
+    ));
+  }
 
-	componentDidMount() {
-	}
+  componentDidMount() {}
 
-	render() {
-
-		return (
-			<View >
-				<MapView
-					style={styles.container}
-					initialRegion={this.state.region}
-					clusterColor={"#6495ED"}
-					showsUserLocation={true}
-					showsMyLocationButton={true}
-					showsBuildings={true}>
-					{this.renderMarkers()}
-				</MapView>
-
-			</View>
-		)
-	}
+  render() {
+    return (
+      <View>
+        <MapView
+          style={styles.container}
+          initialRegion={this.state.region}
+          clusterColor={'#6495ED'}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          showsBuildings={true}>
+          {this.renderMarkers()}
+        </MapView>
+      </View>
+    );
+  }
 }
-
 
 const styles = {
-	container: {
-		width: '100%',
-		height: '100%',
-	},
-	popupView: {
-		flexDirection: "column",
-		backgroundColor: 'white',
-		borderRadius: 10,
-		width: 200,
-	},
-	titlePopup: {
-		fontWeight: "bold",
-		fontFamily: 'Roboto',
-		marginTop: 10,
-		marginLeft: 10,
-		marginRight: 10,
-	},
-	descrPopup: {
-		fontWeight: "normal",
-		fontFamily: 'Roboto',
-		fontSize: 18,
-	},
-	bottomLeftSide: {
-		flexDirection: 'row',
-		marginTop: 10,
-		marginBottom: 10,
-		marginLeft: 10,
-		verticalAlign: 'center',
-		alignContent: 'center',
-	},
-	bottomLeftIcon: {
-		marginLeft: 5,
-	},
-	separator: {
-		marginLeft: 5,
-		marginRight: 5,
-	},
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  popupView: {
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: 200,
+  },
+  titlePopup: {
+    fontWeight: 'bold',
+    fontFamily: 'Roboto',
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  descrPopup: {
+    fontWeight: 'normal',
+    fontFamily: 'Roboto',
+    fontSize: 18,
+  },
+  bottomLeftSide: {
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    verticalAlign: 'center',
+    alignContent: 'center',
+  },
+  bottomLeftIcon: {
+    marginLeft: 5,
+  },
+  separator: {
+    marginLeft: 5,
+    marginRight: 5,
+  },
 };
